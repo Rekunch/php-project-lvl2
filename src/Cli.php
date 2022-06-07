ace948d94cf38baf221dbc6a362338c178b9d95c
Rekunch <95749296+Rekunch@users.noreply.github.com>
2022-06-07 11:39:40 +0300

    Update Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -1,54 +1,33 @@
 <?php
 
 
-
 namespace Differ\Cli;
-
 
 
 use Docopt;
 
 
-
 function start(): string
-
 {
-
     $doc = <<<DOC
-
     Generate diff
 
 
-
     Usage:
-
       gendiff.php (-h|--help)
-
       gendiff.php (-v|--version)
-
       gendiff.php [--format <fmt>] <firstFile> <secondFile>
 
 
-
     Options:
-
       -h --help                     Show this screen
-
       -v --version                  Show version
-
       --format <fmt>                Report format [default: stylish]
-
     DOC;
 
 
-
     $result = Docopt::handle($doc, array('version' => '0.0.1'));
-
     $diff = genDiff($result->args["<firstFile>"], $result->args["<secondFile>"], $result->args["--format"]);
-
     echo $diff;
-
     echo "\n";
-
 }
-
