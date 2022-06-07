ad2347bcc10ba8f365c4e64018a8f80ef30a8d22
Алексей Кривенцов <rekunch1@gmail.com>
2022-06-07 16:35:34 +0300

    complete step one

--- src/Cli.php
+++ src/Cli.php
@@ -1,30 +1,25 @@
 <?php
 
-
 namespace Differ\Cli;
-
 
 use Docopt;
 
-
 function start(): string
 {
-    $doc = <<<DOC
-    Generate diff
-
+    $doc = <<<'DOCOPT'
+Generate diff
 
     Usage:
-      gendiff.php (-h|--help)
-      gendiff.php (-v|--version)
-      gendiff.php [--format <fmt>] <firstFile> <secondFile>
-
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
 
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
       --format <fmt>                Report format [default: stylish]
-    DOC;
 
+DOCOPT;
 
     $result = Docopt::handle($doc, array('version' => '0.0.1'));
     $diff = genDiff($result->args["<firstFile>"], $result->args["<secondFile>"], $result->args["--format"]);
